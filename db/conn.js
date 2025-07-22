f0d60460457b63a076a8f36e3339a38d79541b20
allesonsales <allesonsales@gmail.com>
2025-07-22 12:43:02 -0300

    teste

--- db/conn.js
+++ db/conn.js
@@ -1,16 +1,11 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
-  }
-);
+const sequelize = new Sequelize(process.env.DB_URL, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "mysql",
+});
 
 try {
   sequelize.authenticate();
